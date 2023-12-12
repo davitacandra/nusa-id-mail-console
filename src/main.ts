24807d20b43102395c130d763af816a3f7fea87b
davita <davita@nusa.net.id>
2023-12-12 13:55:30 +0700

    revise headers

--- src/main.ts
+++ src/main.ts
@@ -40,9 +40,9 @@ app.use(bodyParser.json())
 
 app.use(function(req: Request, res: Response, next: NextFunction) {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Methods', req.method)
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
-  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.removeHeader('x-powered-by')
   next()
 })
 
